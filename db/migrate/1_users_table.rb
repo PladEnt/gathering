02c99736bef15790955e521f79ad61451d75f938
PladEnt <joshuadforrest@gmail.com>
2019-03-22 13:39:01 -0700

    fix indentation in user table

--- db/migrate/1_users_table.rb
+++ db/migrate/1_users_table.rb
@@ -1,11 +1,11 @@
 class UsersTable < ActiveRecord::Migration
-    def change
-      create_table :users do |t|
-        t.string :username
-        t.text :email
-        t.string :password_digest
-  
-        t.timestamps null: false
-      end
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.text :email
+      t.string :password_digest
+
+      t.timestamps null: false
     end
   end
+end
